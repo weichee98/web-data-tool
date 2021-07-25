ad131427940d639077f1d9ebd0ca2c28be2bf11c
weichee98 <56989558+weichee98@users.noreply.github.com>
2021-07-25 15:56:42 +0800

    update homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,34 @@ import homeBackground from "./img/home-bg.jpg";
 // import processBackground from "./img/process-bg.jpg";
 import visualizeBackground from "./img/visualize-bg.jpg";
 
+function MainMenuButton(title, content, background, href) {
+  return (
+    <div className="card-body main-menu">
+      <a
+        className="btn card text-center main-menu-button"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPositionY: "center",
+        }}
+        href={href}
+      >
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "2rem 0",
+          }}
+        >
+          <div className="container display-4 text-white">{title}</div>
+          <p className="container text-white">{content}</p>
+        </div>
+      </a>
+    </div>
+  );
+}
+
 function Home() {
   return (
     <div className="home">
@@ -12,31 +40,20 @@ function Home() {
         style={{ backgroundImage: `url(${homeBackground})` }}
       >
         <div className="container">
-          <h1 className="display-2 text-white">
-            Data Processing and Visualization Tool
-          </h1>
+          <h1 className="display-2 text-white">Web Data Tool</h1>
         </div>
       </div>
-      <div className="bg-light index-content">
-        <div className="card-body main-menu">
-          <a
-            className="btn card text-center main-menu-button"
-            style={{
-              backgroundImage: `url(${visualizeBackground})`,
-              backgroundSize: "cover",
-            }}
-            href="/visualize"
-          >
-            <h2 className="display-4 text-white">Data Visualization</h2>
-            <p className="text-white">
-              Plot various kinds of graphs such as line graphs, bar charts,
-              scatter plots and etc to visualize the content of imported data.
-              Plots are interactive such that they are able to be zoomed,
-              dragged and hover across for viewing data values. Plots can also
-              be downloaded as PNG files.
-            </p>
-          </a>
-        </div>
+      <div className="bg-dark index-content">
+        {MainMenuButton(
+          "Data Visualization",
+          `Plot various kinds of graphs such as line graphs, bar charts,
+            scatter plots and etc to visualize the content of imported data.
+            Plots are interactive such that they are able to be zoomed,
+            dragged and hover across for viewing data values. Plots can also
+            be downloaded as PNG files.`,
+          visualizeBackground,
+          "/visualize"
+        )}
       </div>
     </div>
   );
